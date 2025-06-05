6fd149e05158af221509bb76bf9b62600fb48485
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-05 04:23:41 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -7,3 +7,31 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/square_root/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/payment/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/random/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
