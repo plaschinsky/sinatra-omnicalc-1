d49583c242f3ec220b6e358924852c0529dbbe9c
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-05 17:18:31 +0000

    square root done

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
- erb :index
+  @sq_new_number = params[:sq_new_number].to_f
+    @squared = @sq_new_number * @sq_new_number
+ erb :square
 end
 
 get("/square/new") do
@@ -18,7 +20,15 @@ get("/square/results") do
 end
 
 get("/square_root/new") do
-  erb :index
+    @sq_root_new_number = params[:sq_root_new_number].to_f
+    @sq_root = Math.sqrt(@sq_root_new_number)
+  erb :square_root
+end
+
+get("/square_root/results") do
+    @sq_root_new_number = params[:sq_root_new_number].to_f
+    @sq_root = Math.sqrt(@sq_root_new_number)
+  erb :square_root
 end
 
 get("/payment/new") do
